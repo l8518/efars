e233bd675f5d6701be38d77184733ddf829205fa
l8518 <35725355+l8518@users.noreply.github.com>
2019-03-18 18:34:16 +0100

    Add documentation to MLDA

--- efars/plugins/movielens_adapter.py
+++ efars/plugins/movielens_adapter.py
@@ -5,11 +5,20 @@ import pathlib
 
 
 class MovieLensDatasetAdapter(DatasetAdapter):
+    """
+    The MovieLensDatasetAdapter splits a
+    MovieLens Ratings File into test and train set
+    based on the proposed splitting methodology.
+    """
 
     def __init__(self):
+        """ Initializes a new MovieLensDatasetAdapter """
         super().__init__()
 
-    def generate(self, source_file_path, target_folder, seed, test_split_ratio, rating_threshold, max_user_n):
+    def generate(self, source_file_path, target_folder, seed, test_split_ratio,
+                 rating_threshold, max_user_n):
+        """ Generates a test and train dataset based on the largest user
+         profiles and and a 50% user-based split"""
 
         pathlib.Path(target_folder).mkdir(
             parents=True, exist_ok=True)
@@ -20,35 +29,38 @@ class MovieLensDatasetAdapter(DatasetAdapter):
         testir_filepath = os.path.join(
             target_folder, "test_irrelevant_items.csv")
 
-        # load dataset
+        # Load MovieLens Dataset
         df = pandas.read_csv(source_file_path)
-
         df_len = len(df)
 
-        # amount of items (e.g. ratings) to reach test_split size
+        # Calculate the target size, based on the given split ratio
+        # e.g. 20%, that should be reached by later user-based splitting
         trg_size_by_ratings = len(df) * test_split_ratio
-
+        # Count the user profile length, as each movieId represents a rating
         df_evt_cnt = df[['userId', 'movieId']].groupby(['userId'])['movieId'] \
             .count() \
             .reset_index(name='count') \
             .sort_values(['count'], ascending=False)
 
-        # cumsum count
+        # Calculate the cumsum of the user profile lengths
         df_evt_cnt['cumsum'] = df_evt_cnt['count'].cumsum()
 
-        # calculate users, to reach trg_size_by_ratings
-        users_reach_split = df_evt_cnt.loc[df_evt_cnt['cumsum']
-                                           <= trg_size_by_ratings]
+        # Include the number of users to reach the target split size (e.g. 20%)
+        users_reach_split = df_evt_cnt \
+            .loc[df_evt_cnt['cumsum'] <= trg_size_by_ratings]
         su = users_reach_split.sample(n=max_user_n, random_state=seed)
-        # for each user take 50% of items
-        df = df[df['userId'].isin(su['userId'])]
 
+        # Split every selected user into 50% test and train set
+        df = df[df['userId'].isin(su['userId'])]
         test_df = pandas.DataFrame()
         for index, user in su.iterrows():
             items = df[df['userId'] == user['userId']]
             # random sampling from 0.5 of the user items
             u_testset = items.sample(frac=0.5, random_state=seed)
             test_df = test_df.append(u_testset)
+
+        # Remove all ratings selected from the whole set
+        # to obtain the training dataset
         trainset = pandas.read_csv(source_file_path).drop(test_df.index)
         len_tdf = len(test_df)
         len_trainset = len(trainset)
@@ -59,14 +71,16 @@ class MovieLensDatasetAdapter(DatasetAdapter):
             "Amount of Ratings in whole Train + Test Datasets: {0}"
             .format(len_tdf + len_trainset))
         print("Amount of Ratings in whole Dataset: {0}".format(df_len))
-        # split test_df into relevant and non relevant items
 
+        # Additionally calculate the relevant and irrelevant sets
+        # for the test set
         relevant_items = test_df.loc[test_df['rating'].astype(
             'float64') >= rating_threshold]
         irrelevant_items = test_df.loc[test_df['rating'].astype(
             'float64') < rating_threshold]
 
-        # shuffle the train set
+        # Shuffle the trainset by sampling the whole dataset
+        # as MovieLens ratings file is sorted by user id
         trainset = trainset.sample(frac=1.0, random_state=seed)
         # write to csv
         trainset.to_csv(train_filepath, header=False, index=False)
